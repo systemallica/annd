226bca2109125a3f7d08ed2e9e8891dec9f16f31
Andrés Reverón Molina <andressser@gmail.com>
2020-03-12 15:29:48 +0100

    refactor

--- Exercise 3/code/PCA.m
+++ Exercise 3/code/PCA.m
@@ -1,18 +1,14 @@
-% 50x500 matrix with random Gaussian numbers
-% = 500 datapoints of dimension 50
-p = 50;
-X = randn(p,500);
+function [eigenvalues, reducedDataset, reconstructedDataset] = PCA(q,X)
+% PCA 
 % calculate covariance matrix of dataset X
 covMatrix = cov(X');
 % returns q largest eigenvalues in the matrix(D), and the p×q matrix(V) 
 % with the corresponding eigenvectors of the square matrix
-q = 20;
 [eigenvectors,eigenvalues]=eigs(covMatrix,q);
 eigenvalues = diag(eigenvalues);
 
 reducedDataset = (X' * eigenvectors)';
 
 reconstructedDataset = (eigenvectors * reducedDataset) + mean(X);
+end
 
-% estimation of the error
-error = sqrt(mean(mean((X-reconstructedDataset).^2)));
