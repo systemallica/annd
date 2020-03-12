97f1a711482174539a8166780b62a633ad1a1f42
Andrés Reverón Molina <andressser@gmail.com>
2020-03-12 15:10:37 +0100

    PCA algorithm

---
+++ Exercise 3/code/sesssion31.m
@@ -0,0 +1,16 @@
+% 50x500 matrix with random Gaussian numbers
+% = 500 datapoints of dimension 50
+p = 50;
+X = randn(p,500);
+% calculate covariance matrix of dataset X
+covMatrix = cov(X');
+% returns q largest eigenvalues in the matrix(D), and the p×q matrix(V) 
+% with the corresponding eigenvectors of the square matrix
+q = 20;
+[eigenvectors,eigenvalues]=eigs(covMatrix,q);
+eigenvalues = diag(eigenvalues);
+
+reducedDataset = (X' * eigenvectors)';
+
+% estimation of the error
+%sqrt(mean(mean((X-Xhat).^2)));
